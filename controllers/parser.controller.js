9c32b0c37a64b37ab4f03c52cb91a8aaea62aae3
iamangelmp <alexis.mplt@outlook.es>
2023-03-18 00:21:18 -0600

    parser controller using data from xlxs

--- controllers/parser.controller.js
+++ controllers/parser.controller.js
@@ -1,85 +1,107 @@
-const axios = require('axios')
-const createInstance = require('../config/conn.js')
+const dictionary = require("../models/nosql/dictionary.model.js");
+const XLSX = require("xlsx");
 
+const checkMessage = async (req, res, next) => {
+  const msg = req.body.msg;
+  const dataArray = await proccessData(msg);
 
-/**
- * Consulta de pólizas por NIT
- * ej: 4548335-3
-*/
-const findPolizasClienteByNit = async (req, res, next) => {
-  try {
-    const pNIT = req.params.pNIT;
-    const instance = await createInstance();
-    const { data:{data} } = await instance.get('/findPolizasClienteByNit', { params: { pNIT: pNIT } });
-  
-    res.json(data[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error');
+  // si la answare de dataArray es un mensaje de error, regresar un objeto JSON con estatus 500
+  if (typeof dataArray.answare == "string") {
+    return res.status(500).json({ error: dataArray });
   }
-}
 
+  res.json(dataArray);
+};
 
+//covierte cadenas a arreglos
+const convertArray = (msj) => {
+  return String(msj).split(",");
+};
 
-/**
- * Consulta de póliza con requerimientos por NIT
- * 4548335-3
-*/
-const findPolizaRequerimientosByNIT = async(req, res, next)=>{
+//El objeto que retorna la petición
+const proccessData = async (msj) => {
   try {
-    const pNIT = req.params.pNIT;
-    const instance = await createInstance();
-    const {data:{data}} = await instance.get('/findPolizaRequerimientosByNIT', { params: { pNIT: pNIT } });
-    
-    res.json(data[0])
+    const keyword = await dictionary.find({});
+    let dataMsgObj = {
+      message: msj,
+      keywords: [],
+      question: [],
+      answare: [],
+      comments: [],
+    };
+
+    for (faq of keyword) {
+      for (let i = 0; i <= faq.keywords.length; i++) {
+        if (msj.includes(faq.keywords[i]) && msj != "" && msj != " ") {
+          dataMsgObj.answare.push(faq.answare);
+          dataMsgObj.comments.push(faq.comments);
+          dataMsgObj.question.push(faq.question);
+          dataMsgObj.keywords.push(faq.keywords);
+        }
+      }
+    }
+
+    return processAnsware(dataMsgObj);
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Error');
+    return processAnsware({ error: error });
   }
-}
+};
 
+const processAnsware = (dataObj) => {
+  let msg = `No he podido entender tu requerimiento ☹️.En unos momentos uno de nuestros asesores te va a apoyar con tu requerimiento`;
 
+  //elimina respuestas, preguntas y comentarios duplicados
+  dataObj.question = deleteDuplicates([...dataObj.question]);
+  dataObj.comments = deleteDuplicates([...dataObj.comments]);
 
-/**
- * Consulta Póliza con requerimiento por DPI
- * 2423632530101
-*/
-const findPolizaRequerimientosByDPI = async(req, res, next)=>{
-  try {
-    const pDPI = req.params.pDPI;
-    const instance = await createInstance();
-    const {data:{data}} = await instance.get('/findPolizaRequerimientosByDPI', { params: { pDPI: pDPI } });  
-    
-    res.json(data[0])
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error');
-  }
-  
-}
+  dataObj.answare.length == 0
+    ? (dataObj.answare = msg)
+    : (dataObj.answare = deleteDuplicates([...dataObj.answare]));
 
+  return dataObj;
+};
 
+const deleteDuplicates = (array_duplied) => {
+  return array_duplied.filter((item, index) => {
+    return array_duplied.indexOf(item) === index;
+  });
+};
 
-/**
- * Consulta Póliza con requerimiento por DPI
- * 2423632530101
-*/
-const findRequerimientosByLlavePoliza = async(req, res, next)=>{
-  try {
-    const pLlavePoliza = req.params.pLlavePoliza;
-    const instance = await createInstance();
-    const {data:{data}} = await instance.get('/findRequerimientosByLlavePoliza', { params: { pLlavePoliza: pLlavePoliza } });  
-    
-    res.json(data[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error');
-  }
-  
-}
+const createDictionary = async (req, res, next) => {
+  const file = req.files.file;
 
+  var workbook = XLSX.read(file.data);
+  var wsnames = workbook.SheetNames[0];
+  var worksheet = workbook.Sheets[wsnames];
 
+  var jsa = await parseData(XLSX.utils.sheet_to_json(worksheet));
 
+  await dictionary.insertMany(jsa);
 
+  return res.json(jsa);
+};
 
-module.exports = { findPolizasClienteByNit, findPolizaRequerimientosByNIT, findPolizaRequerimientosByDPI, findRequerimientosByLlavePoliza };
+const parseData = async (objects_array) => {
+  let objects_parsed = [];
+  objects_array.forEach((element) => {
+    const {
+      ID,
+      Pregunta,
+      Respuesta,
+      Keywrod,
+      "Comentarios Seguros el Roble": comments,
+    } = element;
+    let parsed_array = {
+      ID: ID,
+      question: Pregunta,
+      answare: Respuesta,
+      keywords: convertArray(Keywrod),
+      comments: comments,
+    };
+    objects_parsed.push(parsed_array);
+  });
+
+  return objects_parsed;
+};
+
+module.exports = { checkMessage, createDictionary };
